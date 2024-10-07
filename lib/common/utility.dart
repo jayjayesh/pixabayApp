91efc205c6bff96f59e64241a3765d48a9765900
lathiyajayesh77 <lathiyajayesh77@gmail.com>
2024-10-07 21:09:05 +0530

    initial first commit : pixabay app

---
+++ lib/common/utility.dart
@@ -0,0 +1,8 @@
+import 'package:intl/intl.dart';
+
+class AppUtility {
+  static String formatNumberToCompactNumber(int views) {
+  final formatter = NumberFormat.compact();
+  return formatter.format(views);
+}
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/features/pixabay/presentation/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
